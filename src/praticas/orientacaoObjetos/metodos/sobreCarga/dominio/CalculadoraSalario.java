c9ba6d97b85bf086a1f89063615a0c08221f28e2
Marquinhos-c <marquinhos.cs08@gmail.com>
2025-07-18 10:17:07 -0400

    Pratica sobrecarga de metodos

---
+++ src/praticas/orientacaoObjetos/metodos/sobreCarga/dominio/CalculadoraSalario.java
@@ -0,0 +1,18 @@
+package praticas.orientacaoObjetos.metodos.sobreCarga.dominio;
+// sobrecarga de metodos
+public class CalculadoraSalario {
+
+    public double calcularSalario (double salarioBase) {
+        return salarioBase;
+    }
+    public double calcularSalario (double salarioBase, double bonus) {
+        return salarioBase + bonus;
+    }
+
+    public double calcularSalario (double salarioBase, int diasFaltados) {
+        double descontoPorFalta = 0.02;
+        double percentualTotalDesconto = descontoPorFalta * diasFaltados;
+        double valorDesconto = salarioBase * percentualTotalDesconto;
+        return salarioBase - valorDesconto;
+    }
+}
---
+++ src/praticas/orientacaoObjetos/metodos/sobreCarga/test/CalculadoraSalarioTest.java
@@ -0,0 +1,13 @@
+package praticas.orientacaoObjetos.metodos.sobreCarga.test;
+
+import praticas.orientacaoObjetos.metodos.sobreCarga.dominio.CalculadoraSalario;
+
+public class CalculadoraSalarioTest {
+    public static void main(String[] args) {
+        CalculadoraSalario calculadora = new CalculadoraSalario();
+
+        System.out.println("Salário base: " + calculadora.calcularSalario(3000));
+        System.out.println("Salário com bônus: " + calculadora.calcularSalario(3000, 300.0));
+        System.out.println("Salário com 3 falta: " + calculadora.calcularSalario(3000, 3));
+    }
+}
