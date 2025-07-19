433b6f5a8403ebaf1848cc5234b228b9975ff152
Marquinhos-c <marquinhos.cs08@gmail.com>
2025-07-19 10:02:16 -0400

    Renomeiando pastas

--- src/orientacaoObjetos/metodos/parametros/dominio/Estudante.java
+++
@@ -1,8 +0,0 @@
-package orientacaoObjetos.metodos.parametros.dominio;
-
-// Classe que representa um estudante com nome, idade e sexo
-public class Estudante {
-    public String nome; // Nome do estudante
-    public int idade; // Idade do estudante
-    public char sexo; // Sexo do estudante (M ou F)
-}
