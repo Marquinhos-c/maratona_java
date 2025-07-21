dc07430ff264ea25a4c546b4fb269df7987c29b3
Marquinhos-c <marquinhos.cs08@gmail.com>
2025-07-21 12:37:10 -0400

    Modificadores estáticos

---
+++ src/orientacaoObjetos/Metodos/modificadoresEstaticos/domain/Carro02.java
@@ -0,0 +1,43 @@
+package orientacaoObjetos.metodos.modificadoresEstaticos.domain;
+
+public class Carro02 {
+    private String nome;
+    private double velocidadeMaxima;
+    private static double velocidadeLimite = 250;
+
+    // Construtor da classe Carro02.
+    public Carro02(String nome, double velocidadeMaxima) {
+        this.nome = nome;
+        this.velocidadeMaxima = velocidadeMaxima;
+    }
+
+    // Método de instância que imprime as informações do carro.
+    // Exibe o nome, a velocidade máxima individual e a velocidade limite global.
+    public void imprime() {
+        System.out.println("-------------------");
+        System.out.println("Nome: " + this.nome);
+        System.out.println("Velocidade maxima: "+ this.velocidadeMaxima);
+        System.out.println("Velocidade limite: " + Carro02.velocidadeLimite);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getVelocidadeMaxima() {
+        return velocidadeMaxima;
+    }
+    public void setVelocidadeMaxima(double velocidadeMaxima) {
+        this.velocidadeMaxima = velocidadeMaxima;
+    }
+
+    public static double getVelocidadeLimite() {
+        return velocidadeLimite;
+    }
+    public static void setVelocidadeLimite(double velocidadeLimite) {
+        Carro02.velocidadeLimite = velocidadeLimite;
+    }
+}
---
+++ src/orientacaoObjetos/Metodos/modificadoresEstaticos/test/CarroTest02.java
@@ -0,0 +1,18 @@
+package orientacaoObjetos.metodos.modificadoresEstaticos.test;
+
+import orientacaoObjetos.metodos.modificadoresEstaticos.domain.Carro02;
+
+public class CarroTest02 {
+    public static void main(String[] args) {
+        Carro02.setVelocidadeLimite(180);
+
+        Carro02 c1 = new Carro02("BMW", 280);
+        Carro02 c2 = new Carro02("Mercedes", 275);
+        Carro02 c3 = new Carro02("Audi", 290);
+
+
+        c1.imprime();
+        c2.imprime();
+        c3.imprime();
+    }
+}
