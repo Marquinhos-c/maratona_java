fa0b6a0ed000b738f44f4ef84357d869838e20c6
Marquinhos-c <marquinhos.cs08@gmail.com>
2025-07-22 18:22:11 -0400

    metodos estáticos

---
+++ src/orientacaoObjetos/Metodos/modificadoresEstaticos/domain/Anime.java
@@ -0,0 +1,39 @@
+package orientacaoObjetos.metodos.modificadoresEstaticos.domain;
+
+public class Anime {
+    private String nome;
+    private static int[] episodios;
+
+    static {
+        System.out.println("Dentro do bloca de inicialização static 1 ");
+        episodios = new int[100];
+        for (int i = 0; i < episodios.length; i++) {
+            episodios[i] = i+1;
+        }
+    }
+    static {
+        System.out.println("Dentro do bloca de inicialização static 2 ");
+    }
+
+    {
+        System.out.println("Dentro do bloca de inicialização não static ");
+    }
+
+    // construtor
+    public Anime(String nome) {
+        this.nome = nome;
+    }
+    // construtor
+    public Anime() {
+        for (int episodio: episodios) {
+            System.out.print(episodio + " ");
+        }
+    }
+
+    public void setEpisodio(int[] episodios) {
+        Anime.episodios = episodios;
+    }
+    public int[] getEpisodio() {
+        return episodios;
+    }
+}
---
+++ src/orientacaoObjetos/Metodos/modificadoresEstaticos/test/AnimeTest01.java
@@ -0,0 +1,11 @@
+package orientacaoObjetos.metodos.modificadoresEstaticos.test;
+
+import orientacaoObjetos.metodos.modificadoresEstaticos.domain.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime1 = new Anime();
+        Anime anime2 = new Anime();
+        Anime anime3 = new Anime();
+    }
+}
