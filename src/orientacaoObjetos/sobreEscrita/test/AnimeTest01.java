c91f21e217b49fcc765cd6732866ab602abfe518
Marquinhos-c <marquinhos.cs08@gmail.com>
2025-08-22 12:51:56 -0400

    Praticando sobre escrita com toString

---
+++ src/orientacaoObjetos/sobreEscrita/test/AnimeTest01.java
@@ -0,0 +1,11 @@
+package orientacaoObjetos.sobreEscrita.test;
+
+import orientacaoObjetos.sobreEscrita.dominio.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime("Naruto");
+
+        System.out.println(anime);
+    }
+}
