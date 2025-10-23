682365fa77c16ebac56c2bd2aafe2857633f63c3
Marquinhos-c <marquinhos.cs08@gmail.com>
2025-10-23 08:12:22 -0400

    Alterações nos exercicios

--- src/praticas/orientacaoobjetos/polimorfismo/figure/model/Retangulo.java
+++ src/praticas/orientacaoobjetos/polimorfismo/figure/model/Retangulo.java
@@ -9,11 +9,11 @@ public class Retangulo extends FiguraGeometrica{
         this.largura = largura;
         this.altura = altura;
     }
-
+    @Override
     public double calcularArea() {
         return largura * altura;
     }
-
+    @Override
     public double calcularPerimetro() {
         return 2 * (largura + altura);
     }
