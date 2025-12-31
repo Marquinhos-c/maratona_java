c2591146ae0cc26df15453842bb8d3b8d1068f03
Marquinhos-c <marquinhos.cs08@gmail.com>
2025-12-31 19:26:35 -0400

    Design Patterns - singleton introdução

---
+++ src/designpatterns/singleton/test/AircraftTest01.java
@@ -0,0 +1,18 @@
+package designpatterns.singleton.test;
+
+import designpatterns.singleton.dominio.Aircraft;
+import designpatterns.singleton.dominio.AircraftSingletonEager;
+
+public class AircraftTest01 {
+    public static void main(String[] args) {
+        bookSeat("1A");
+        bookSeat("1A");
+    }
+
+    private static void bookSeat(String seat) {
+        System.out.println(AircraftSingletonEager.getINSTANCE());
+        Aircraft aircraft = new Aircraft("787-900");
+        System.out.println(aircraft.bookSeat(seat));
+    }
+
+}
